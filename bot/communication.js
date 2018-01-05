ce2b0a3608ff1215fef060bf9ea121b97b2293a1
Piotr Ptak <robakzdobywca@gmail.com>
2018-01-05 17:47:00 +0100

    interactive chat with wit.ai, key from resources fix

--- bot/communication.js
+++ bot/communication.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const {Wit, log, interactive} = require('node-wit');
-const _witAccessToken = fs.readFileSync('../keys/wit-access-token.txt', 'utf8').split('\n')[0];
+const _witAccessToken = fs.readFileSync('../resources/keys/wit-access-token.txt', 'utf8').split('\n')[0];
 
 const client = new Wit({
   accessToken: _witAccessToken,
