28f3a8e0fc2790b3a9a28cd68d64d8ee8f7ba028
Piotr Ptak <robakzdobywca@gmail.com>
2018-01-12 18:24:24 +0100

    server run script

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 // server.js
 var app = require('./app');
-var server = require('../resources/serverAddress.json')
-var port = process.env.PORT || ;
+var server = require('../resources/serverAddress.json');
+var port = process.env.PORT || server.port;
 var server = app.listen(server.port, () => {
   console.log('Express server listening on port ' + server.port);
 });
