71851806dc4af404261bc78bf5c9353c743a5636
Piotr Ptak <robakzdobywca@gmail.com>
2018-01-12 18:15:51 +0100

    server address get from resource

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // server.js
 var app = require('./app');
-var port = process.env.PORT || 3000;
-var server = app.listen(port, function() {
-  console.log('Express server listening on port ' + port);
+var server = require('../resources/serverAddress.json')
+var port = process.env.PORT || ;
+var server = app.listen(server.port, () => {
+  console.log('Express server listening on port ' + server.port);
 });
