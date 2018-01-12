b243949b798bc50f6a5def59ef97ca0a03e08ff8
miamiultras <bla>
2018-01-12 18:03:50 +0100

    communication.js fix

--- bot/communication.js
+++ bot/communication.js
@@ -2,9 +2,13 @@ const fs = require('fs');
 const { Wit, log, interactive } = require('node-wit');
 const _witAccessToken = fs.readFileSync('../resources/keys/wit-access-token.txt', 'utf8').split('\n')[0];
 
-var express = require('express');
-var router = express.Router();
-var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://maciejkrol:password@ds135817.mlab.com:35817/kruche_krolestwo');
+const Entity = require('../server/models/entity');
+
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: true }));
 
 const client = new Wit({
@@ -13,18 +17,46 @@ const client = new Wit({
 });
 
 router.get('/message', function (req, res) {
-  console.log(req.query.name);
+  //console.log(req.query.name);
   getJSON(req.query.name, res);
 });
 
 function getJSON(keyword, res) {
   client.message(keyword, {})
     .then(data => {
-      console.log('~ response: ' + JSON.stringify(data));
-      res.send(JSON.stringify(data));
+      findEntityByType(data.entities.smalltalk[0].value);
+      //console.log('~ response: ' + JSON.stringify(data));
+      //res.send(JSON.stringify(data));
     })
     .catch(console.error);
 }
 
+function findEntityByType(type) {
+  console.log(type)
+  Entity.findOne({ name: type }, (err, res) => {
+    console.log(res.intents);
+  })
+}
 
+function insertEntity(name, intents) {
+  const entity = new Entity({
+    name: name,
+    intents: intents
+  });
+  entity.save()
+    .then(() => {
+      console.log("entity saved");
+    })
+    .catch((err) => console.log(err));
+  console.log(entity);
+}
+
+// insertEntity("introduce", 
+// [
+//   "jestem navi... jestem by pomoc ci odnalezc swoje miejsce",
+//   "yo yo tu navi, co chcesz?",
+//   "tu navi odbior, czego chcesz?"
+// ]);
+
+findEntityByType("greet");
 module.exports = router;
