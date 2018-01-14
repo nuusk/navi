78d026a32bf6787309dd7da2ae02112d71a4bb87
Piotr Ptak <robakzdobywca@gmail.com>
2018-01-14 21:28:32 +0100

    no serio

--- src/views/Main.js
+++ src/views/Main.js
@@ -38,7 +38,7 @@ class Main extends Component {
     console.log(this.state.latitude);
     console.log(this.state.longitude);
     fetch('http://localhost:9004/api/message?name='+encodeURIComponent(query)
-    +'&lat=this.state.latitude='+this.state.latitude
+    +'&lat='+this.state.latitude
     +'&lng='+this.state.longitude, {
       method: 'GET',
       headers: {
