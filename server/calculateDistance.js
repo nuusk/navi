64a8aa6aa2ddf4ef69fc3b82944861c12d9cf7b6
Piotr Ptak <robakzdobywca@gmail.com>
2018-01-17 11:52:22 +0100

    calculator class

--- server/calculateDistance.js
+++ server/calculateDistance.js
@@ -1,22 +1,25 @@
-Number.prototype.toRadians = function() {
-  console.log(this);
-  return this * Math.PI / 180;
+class Calculator() {
+
+  Number.prototype.toRadians = function() {
+    console.log(this);
+    return this * Math.PI / 180;
+  }
+
+  function calculateDistance(lat1, lng1, lat2, lng2) {
+    const R = 6371e3; // metres
+    const lat1Radians = lat1.toRadians();
+    const lat2Radians = lat2.toRadians();
+    const latDelta = (lat2-lat1).toRadians();
+    const lngDelta = (lng2-lng1).toRadians();
+
+    const a = Math.sin(latDelta/2) * Math.sin(latDelta/2) +
+            Math.cos(lat1Radians) * Math.cos(lat2Radians) *
+            Math.sin(lngDelta/2) * Math.sin(lngDelta/2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    const d = R * c;
+    return d;
+  }
 }
 
-function calculateDistance(lat1, lon1, lat2, lon2) {
-  const R = 6371e3; // metres
-  const lat1Radians = lat1.toRadians();
-  const lat2Radians = lat2.toRadians();
-  const latDelta = (lat2-lat1).toRadians();
-  const lonDelta = (lon2-lon1).toRadians();
-
-  const a = Math.sin(latDelta/2) * Math.sin(latDelta/2) +
-          Math.cos(lat1Radians) * Math.cos(lat2Radians) *
-          Math.sin(lonDelta/2) * Math.sin(lonDelta/2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-
-  const d = R * c;
-  return d;
-}
-
-module.exports = calculateDistance;
+module.exports = Calculator;
