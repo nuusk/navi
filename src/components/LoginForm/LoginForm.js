93d4d74d8c34229908f2447f14a8c06bd054b996
Piotr Ptak <robakzdobywca@gmail.com>
2018-02-18 17:29:40 +0100

    login form

---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './LoginForm.css';
+
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="LoginForm">
+        <form id="registerForm">
+          <input id="email" name="email" type="text" placeholder="your@email.com" required />
+          <input id="password" name="password" type="password" placeholder="yourpassword" required />
+        </form>
+        <button type="button" onClick={this.props.onSubmit}>Login</button>
+      </div>
+    );
+  }
+}
+
+
+export default LoginForm;
